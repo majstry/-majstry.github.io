28e9a7ff9a56c4df063c31fb6ab87ab8f86e02d4
Artem Mezin <iketari@gmail.com>
2015-07-31 19:59:43 +0300

    emails app

--- assets/js/grayscale.js
+++ assets/js/grayscale.js
@@ -1,137 +1,87 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
 //jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+// $(function() {
+//     $('.page-scroll a').bind('click', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: $($anchor.attr('href')).offset().top
+//         }, 1500, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+// });
+
+// A simple email model
+var EmailModel = Backbone.Model.extend({
+  defaults: { email: '' },
+  validate: function (attributes, options) {
+    if (!/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(attributes.email)) {
+      return 'invalid_email';
+    }
+  }
 });
 
-//Google Map Skin - Get more at http://snazzymaps.com/
-// var myOptions = {
-//     zoom: 15,
-//     center: new google.maps.LatLng(53.385873, -1.471471),
-//     mapTypeId: google.maps.MapTypeId.ROADMAP,
-//     disableDefaultUI: true,
-//     styles: [{
-//         "featureType": "water",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 17
-//         }]
-//     }, {
-//         "featureType": "landscape",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 20
-//         }]
-//     }, {
-//         "featureType": "road.highway",
-//         "elementType": "geometry.fill",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 17
-//         }]
-//     }, {
-//         "featureType": "road.highway",
-//         "elementType": "geometry.stroke",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 29
-//         }, {
-//             "weight": 0.2
-//         }]
-//     }, {
-//         "featureType": "road.arterial",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 18
-//         }]
-//     }, {
-//         "featureType": "road.local",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 16
-//         }]
-//     }, {
-//         "featureType": "poi",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 21
-//         }]
-//     }, {
-//         "elementType": "labels.text.stroke",
-//         "stylers": [{
-//             "visibility": "on"
-//         }, {
-//             "color": "#000000"
-//         }, {
-//             "lightness": 16
-//         }]
-//     }, {
-//         "elementType": "labels.text.fill",
-//         "stylers": [{
-//             "saturation": 36
-//         }, {
-//             "color": "#000000"
-//         }, {
-//             "lightness": 40
-//         }]
-//     }, {
-//         "elementType": "labels.icon",
-//         "stylers": [{
-//             "visibility": "off"
-//         }]
-//     }, {
-//         "featureType": "transit",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 19
-//         }]
-//     }, {
-//         "featureType": "administrative",
-//         "elementType": "geometry.fill",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 20
-//         }]
-//     }, {
-//         "featureType": "administrative",
-//         "elementType": "geometry.stroke",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 17
-//         }, {
-//             "weight": 1.2
-//         }]
-//     }]
-// };
 
-// var map = new google.maps.Map(document.getElementById('map'), myOptions);
+// Create a Firebase.Collection and set the 'firebase' property
+// to the URL of our database
+var EmailCollection = Backbone.Firebase.Collection.extend({
+  model: EmailModel,
+  url: "https://majstry.firebaseio.com/contacts"
+});
+
+
+var App = Backbone.View.extend({
+  el: $('#page-top'),
+  events: {
+    'click .page-scroll a': 'scrollNext',
+    'submit #email_form': 'submitEmail',
+    'click .js-submit': 'submitEmail'
+  },
+
+  initialize: function () {
+    this.$form = this.$('#email_form');
+    this.$inputGroup = this.$form.find('.input-group');
+    this.$input = this.$form.find('input');
+
+
+    this.$input.on('input', _.bind(function (event) {
+        this.$inputGroup.removeClass('has-error');
+    }, this));
+
+    this.collection = new EmailCollection;
+  },
+
+  scrollNext: function (event) {
+    event.preventDefault();
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+  },
+
+  submitEmail: function (event) {
+    event.preventDefault();
+
+    var model = new EmailModel({
+      email: this.$input.val()
+    })
+
+    if (model.isValid()) {
+      this.collection.create(model.toJSON());
+      this.renderThanks();
+    } else {
+      this.renderError(model);
+    }
+  },
+
+  renderThanks: function () {
+    this.$('.newsletter-box').hide();
+    this.$('.js-success-submit').show();
+  },
+
+  renderError: function (model) {
+    this.$inputGroup.addClass('has-error');
+  }
+});
+
+$(function () {
+  window.app = new App;
+})
